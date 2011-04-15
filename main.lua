a675ae235a2a0bffc16c6446f79c2fd5af4bba0a
dusknoir <me@dusknoir.net>
2011-04-15 02:52:43 -0400

    for the dorks without addonloader

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
+if select(2, UnitClass "player") ~= "PRIEST" then return end
+
 local GetShapeshiftForm = GetShapeshiftForm
 local SetCVar = SetCVar
 
 local ef = CreateFrame("frame")
 ef:RegisterEvent("UPDATE_SHAPESHIFT_FORM")
-ef:SetScript("OnEvent", function()
-    SetCVar("CombatHealing", GetShapeshiftForm() == 0 and "1" or "0")
-end)
+ef:SetScript("OnEvent", function() SetCVar("CombatHealing", GetShapeshiftForm() == 0 and "1" or "0") end)
